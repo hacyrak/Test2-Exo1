d3dfdf52e8615f495f3de23c001e8b7342ae67a5
a1007744 <a1007744@a-f004249.salsa.univ-amu.fr>
2019-02-28 16:03:46 +0100

    Construction de la classe RomanToNumeral

--- Test2-Exo1/src/RomanToNumeral.java
+++ Test2-Exo1/src/RomanToNumeral.java
@@ -1,4 +1,29 @@
+import java.util.HashMap;
 
 public class RomanToNumeral {
-
+	protected HashMap<String,Integer> Ci = new HashMap<String, Integer>();
+	public RomanToNumeral(){
+		this.Ci.put("I",1);
+		this.Ci.put("V", 5);
+		this.Ci.put("X", 10);
+		this.Ci.put("L", 50);
+		this.Ci.put("C",100);
+		this.Ci.put("D", 500);
+		this.Ci.put("M", 1000);
+	}
+	
+	public int getNumeral(String chiffre) {
+		int res = 0;
+		for (int i = 0; i<=chiffre.length()-1; i++) {
+			System.out.println(chiffre.charAt(i));
+		}
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		RomanToNumeral test = new RomanToNumeral();
+		test.getNumeral("XXXIV");
+	}
+	
+	
 }
